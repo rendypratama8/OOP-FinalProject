1835dda834202402b6f2a93ae0d1d1a5c57bb86e
rendypratama8 <rendyputrapratama8@gmail.com>
2018-11-30 19:35:23 +0700

    test push yaaa

--- src/controller/ControllerCariAlatTulis2.java
+++ src/controller/ControllerCariAlatTulis2.java
@@ -74,5 +74,5 @@ public class ControllerCariAlatTulis2 {
         this.view.setLocationRelativeTo(null);
         this.view.show();
     }
-    
+    //----------------------------------------------------
 }
